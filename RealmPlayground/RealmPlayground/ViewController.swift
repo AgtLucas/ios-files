06edf5ca21272536fc41d02535789dfcf67f29b0
Lucas <lucas@agtlucas.com>
2015-07-02 22:33:07 -0300

    Testing Alamofire request

--- RealmPlayground/RealmPlayground/ViewController.swift
+++ RealmPlayground/RealmPlayground/ViewController.swift
@@ -7,17 +7,28 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    getAllLines()
   }
 
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
+  }
+  
+  func getAllLines() {
+    let url = "http://line.voudeonibusapp.com/all"
+    
+    Alamofire.request(.GET, url)
+      .responseJSON { (_, _, JSON, _) in
+        println(JSON)
+      }
   }
 
 
---
+++ RealmPlayground/RealmPlayground/api.swift
@@ -0,0 +1,9 @@
+//
+//  api.swift
+//  RealmPlayground
+//
+//  Created by Lucas da Silva on 7/2/15.
+//  Copyright (c) 2015 Vou de ônibus. All rights reserved.
+//
+
+import Foundation
